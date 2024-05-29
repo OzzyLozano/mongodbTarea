ac59bcc0b14cb7b0e8b2067001939105b89024be
Oziel Lozano <oziellozanouwu@gmail.com>
2024-05-29 13:48:37 -0600

    genre selector done

--- scripts/genreTags.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-  $('#genre-selection').selectpicker();
-});
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+VirtualSelect.init({ 
+  ele: 'select',
+  search: true,
+  searchGroup: false,
+  searchByStartsWith: false,
+  placeholder: 'Seleccione al menos 1 género',
+})
